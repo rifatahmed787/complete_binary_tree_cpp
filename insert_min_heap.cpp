888127c4bfbf02d13d3cdbdfc8f0f9ca024f3baa
Md Rifat <mdrifatahmed787@gmail.com>
2024-09-09 01:08:37 +0600

    min heap insert

---
+++ insert_min_heap.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; i++) //complexity N;
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+        int current_indx = v.size() - 1;
+        // cout<<current_indx<<endl;
+        while (current_indx != 0) //complexity log(N)
+        {
+            int parent_indx = (current_indx - 1) / 2;
+            if (v[parent_indx] > v[current_indx])
+            {
+                swap(v[current_indx], v[parent_indx]);
+            }
+            else
+            {
+                break;
+            };
+            current_indx = parent_indx;
+        }
+    }
+
+    for (int val : v)
+        cout << val << " ";
+    return 0;
+}
+
+// complexity Nlog(N);
