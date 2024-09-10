50a122515a6949524ac48de5801a739ad7ac5517
Md Rifat <mdrifatahmed787@gmail.com>
2024-09-10 16:27:08 +0600

    max heap priority heap

---
+++ priority_queue.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    priority_queue<int>pq;
+
+    while(ture){
+        int c;
+        cin>>c;
+
+        if(c==0){
+            int n;
+            cin>>n;
+            pq.push(n);
+        }else if(c==1){
+            pq.pop();
+        }else if(c==2){
+            cout<<pq.top()<<endl;
+        }else{
+            break;
+        }
+    }
+    return 0;
+}
