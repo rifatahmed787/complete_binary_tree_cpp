fc530996aae782656f685dcff917b1e0302f17ae
Md Rifat <mdrifatahmed787@gmail.com>
2024-09-15 11:57:23 +0600

    shorted

---
+++ Sorted.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t; 
+    cin >> t;
+    while (t--){
+        int n;
+        set<int> s;
+        cin >> n;
+        for(int i = 0; i < n; i++){
+            int x;
+            cin >> x;
+            s.insert(x);
+        }
+        for(auto it: s){
+            cout << it << " ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
